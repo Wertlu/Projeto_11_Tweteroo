d6722ab58443af984793ba65e539d5edc4c89367
Luiz Felipe de J. Santos <luiz_felipe_js@hotmail.com>
2022-01-22 22:49:38 -0300

    Adding Sign-up
    Responding "/sign-up" route

---
+++ serv.js
@@ -0,0 +1,11 @@
+import express, { json } from "express";
+
+const serv = express();
+serv.use(json());
+
+const users = [];
+
+serv.post('/sign-up', (req, res) => {
+    users.push(req.body);
+    res.send("OK");
+})
