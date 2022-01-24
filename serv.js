69ae847599365331626382a9835ef3da6096afdd
Luiz Felipe de J. Santos <luiz_felipe_js@hotmail.com>
2022-01-23 22:43:21 -0300

    Adding cors

--- serv.js
+++ serv.js
@@ -1,13 +1,36 @@
-import express, { json } from "express";
+import express from "express";
+import cors from "cors";
 
 const serv = express();
-serv.use(json());
+serv.use(express.json());
+serv.use(cors());
+
+serv.get("/", (req, res) => {
+    res.send("Running the server");
+});
 
 const users = [];
 
-serv.post('/sign-up', (req, res) => {
+serv.post("/sign-up", (req, res) => {
     users.push(req.body);
     res.send("OK");
+});
+
+const tweets = [];
+
+serv.post("/tweets", (req, res) => {
+    tweets.push(req.body);
+    res.send("OK");
+});
+
+serv.get("/tweets", (req, res) => {
+    const latestsTweets = [];
+
+    for (let i = 0; i <= 10; i++) {
+        latestsTweets.push(tweets[(tweets.length - 1) - i]);
+    }
+
+    res.send(latestsTweets);
 })
 
 serv.listen(5000);
